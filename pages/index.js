317c228982a93d7cf3330b7d62d7142f672f4aad
Filip Jerga <jerga99@gmail.com>
2021-08-30 10:40:51 +0200

    Tailwind integration

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,16 @@
 
 export default function Home() {
   return (
-    <>Hello World</>
+    <>
+      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+        <div className="flex-shrink-0">
+          <img className="h-12 w-12" src="/vercel.svg" alt="ChitChat Logo" />
+        </div>
+        <div>
+          <div className="text-xl font-medium text-black">ChitChat</div>
+          <p className="text-gray-500">You have a new message!</p>
+        </div>
+      </div>
+    </>
   )
 }
