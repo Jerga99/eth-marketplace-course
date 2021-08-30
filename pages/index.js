805b5a3adcc41cb36632d2961336dbc0d41484f7
Filip Jerga <jerga99@gmail.com>
2021-08-30 11:11:09 +0200

    Compiler options

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,11 @@
 
 
+import Test from "@components/sub1/sub2/Test"
+
 export default function Home() {
   return (
     <>
-      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
-        <div className="flex-shrink-0">
-          <img className="h-12 w-12" src="/vercel.svg" alt="ChitChat Logo" />
-        </div>
-        <div>
-          <div className="text-xl font-medium text-black">ChitChat</div>
-          <p className="text-gray-500">You have a new message!</p>
-        </div>
-      </div>
+      <Test />
     </>
   )
 }
---
+++ pages/sub1/Test.js
@@ -0,0 +1,4 @@
+
+
+import Test from "../../components/sub1/sub2/Test"
+import Test from "@components/sub1/sub2/Test"
