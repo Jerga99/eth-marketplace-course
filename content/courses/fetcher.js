bea63caff508bebe615bf52b48342d8c4b2ab9de
Filip Jerga <jerga99@gmail.com>
2021-08-31 09:22:36 +0200

    Course data

---
+++ content/courses/fetcher.js
@@ -0,0 +1,15 @@
+
+
+import courses from "./index.json"
+
+export const getAllCourse = () => {
+
+  return {
+    data: courses,
+    courseMap: courses.reduce((a, c, i) => {
+      a[c.id] = c
+      a[c.id].index = i
+      return a
+    }, {})
+  }
+}
