bc0b39ab897f0d528493f5a4a3c99d5c4db663c4
Filip Jerga <jerga99@gmail.com>
2021-09-02 11:32:29 +0200

    Navbar account

--- components/ui/common/button/index.js
+++ components/ui/common/button/index.js
@@ -4,14 +4,21 @@
 
 export default function Button({
   children,
-  className = "text-white bg-indigo-600 hover:bg-indigo-700",
+  className,
+  hoverable = true,
+  variant = "purple",
   ...rest
 }) {
+
+  const variants = {
+    purple: `text-white bg-indigo-600 ${hoverable && "hover:bg-indigo-700"}`,
+    red: `text-white bg-red-600 ${hoverable && "hover:bg-red-700"}`,
+  }
 
   return (
     <button
       {...rest}
-      className={`disabled:opacity-50 disabled:cursor-not-allowed px-8 py-3 border rounded-md text-base font-medium ${className}`}>
+      className={`disabled:opacity-50 disabled:cursor-not-allowed px-8 py-3 border rounded-md text-base font-medium ${className} ${variants[variant]}`}>
       {children}
     </button>
   )
--- components/ui/common/navbar/index.js
+++ components/ui/common/navbar/index.js
@@ -13,7 +13,6 @@ export default function Footer() {
 
   return (
     <section>
-      {account}
       <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
         <nav className="relative" aria-label="Global">
           <div className="flex justify-between items-center">
@@ -51,9 +50,15 @@ export default function Footer() {
                     Loading...
                 </Button> :
                 isWeb3Loaded ?
+                account ?
+                <Button
+                  hoverable={false}
+                  className="cursor-default">
+                  Hi there
+                </Button> :
                 <Button
                   onClick={connect}>
-                    Connect
+                  Connect
                 </Button> :
                 <Button
                   onClick={() => window.open("https://metamask.io/download.html", "_blank")}>
