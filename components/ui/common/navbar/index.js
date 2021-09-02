40136eb55156474711de6f1532b4a6751de4a98d
Filip Jerga <jerga99@gmail.com>
2021-09-02 15:00:51 +0200

    Display address

--- components/ui/common/navbar/index.js
+++ components/ui/common/navbar/index.js
@@ -13,7 +13,6 @@ export default function Footer() {
 
   return (
     <section>
-      {account}
       <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
         <nav className="relative" aria-label="Global">
           <div className="flex justify-between items-center">
@@ -70,6 +69,13 @@ export default function Footer() {
           </div>
         </nav>
       </div>
+      { account &&
+        <div className="flex justify-end pt-1 sm:px-6 lg:px-8">
+          <div className="text-white bg-indigo-600 rounded-md p-2">
+            {account}
+          </div>
+        </div>
+      }
     </section>
   )
 }
