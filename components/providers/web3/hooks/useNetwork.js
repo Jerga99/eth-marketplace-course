e9b0e2a499e4575d6ac90cfcc7243e06b97c270e
Filip Jerga <jerga99@gmail.com>
2021-09-03 11:37:10 +0200

    Chain ID

--- components/providers/web3/hooks/useNetwork.js
+++ components/providers/web3/hooks/useNetwork.js
@@ -1,20 +1,31 @@
 import { useEffect } from "react"
 import useSWR from "swr"
 
+const NETWORKS = {
+  1: "Ethereum Main Network",
+  3: "Ropsten Test Network",
+  4: "Rinkeby Test Network",
+  5: "Goerli Test Network",
+  42: "Kovan Test Network",
+  56: "Binance Smart Chain",
+  1337: "Ganache",
+}
 
 export const handler = (web3, provider) => () => {
 
   const { mutate, ...rest } = useSWR(() =>
     web3 ? "web3/network" : null,
     async () => {
-      const netId = await web3.eth.net.getId()
-      return netId
+      const chainId = await web3.eth.getChainId()
+      return NETWORKS[chainId]
     }
   )
 
   useEffect(() => {
     provider &&
-    provider.on("chainChanged", netId => mutate(netId))
+    provider.on("chainChanged", chainId => {
+      mutate(NETWORKS[parseInt(chainId, 16)])
+    })
   }, [web3])
 
   return {
--- pages/marketplace/index.js
+++ pages/marketplace/index.js
@@ -14,7 +14,6 @@ export default function Marketplace({courses}) {
   return (
     <>
       <div className="py-4">
-        { network.data }
         <WalletBar
           address={account.data}
           network={network.data}
