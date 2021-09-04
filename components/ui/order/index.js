e08f562d3706d6f5737f65a87edf46d2b34135e4
Filip Jerga <jerga99@gmail.com>
2021-09-04 14:41:02 +0200

    Order modal

--- components/ui/order/index.js
+++ components/ui/order/index.js
@@ -1,3 +1,4 @@
 
 
 export { default as OrderCard } from "./card"
+export { default as OrderModal } from "./modal"
--- pages/marketplace/index.js
+++ pages/marketplace/index.js
@@ -5,7 +5,8 @@ import { BaseLayout } from "@components/ui/layout"
 import { getAllCourses } from "@content/courses/fetcher"
 import { WalletBar } from "@components/ui/web3"
 import { useAccount, useNetwork } from "@components/hooks/web3"
-import { Button, Modal } from "@components/ui/common"
+import { Button } from "@components/ui/common"
+import { OrderModal } from "@components/ui/order"
 
 export default function Marketplace({courses}) {
   const { account } = useAccount()
@@ -41,7 +42,7 @@ export default function Marketplace({courses}) {
         />
       }
       </CourseList>
-      <Modal isOpen={false} />
+      <OrderModal />
     </>
   )
 }
