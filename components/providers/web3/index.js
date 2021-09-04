1ff7b04a135721de65fd2d73f99525f7330c4a8c
Filip Jerga <jerga99@gmail.com>
2021-09-04 13:06:45 +0200

    Setup hooks fix

--- components/providers/web3/index.js
+++ components/providers/web3/index.js
@@ -11,7 +11,8 @@ export default function Web3Provider({children}) {
     provider: null,
     web3: null,
     contract: null,
-    isLoading: true
+    isLoading: true,
+    hooks: setupHooks()
   })
 
   useEffect(() => {
@@ -24,7 +25,8 @@ export default function Web3Provider({children}) {
           provider,
           web3,
           contract: null,
-          isLoading: false
+          isLoading: false,
+          hooks: setupHooks(web3, provider)
         })
       } else {
         setWeb3Api(api => ({...api, isLoading: false}))
@@ -40,7 +42,6 @@ export default function Web3Provider({children}) {
     return {
       ...web3Api,
       isWeb3Loaded: web3 != null,
-      getHooks: () => setupHooks(web3, provider),
       connect: provider ?
         async () => {
           try {
@@ -65,7 +66,7 @@ export function useWeb3() {
 }
 
 export function useHooks(cb) {
-  const { getHooks } = useWeb3()
-  return cb(getHooks())
+  const { hooks } = useWeb3()
+  return cb(hooks)
 }
 
