792a66f9f5c8464dee1bdd022d78b9c9354def45
Filip Jerga <jerga99@gmail.com>
2021-09-06 12:46:27 +0200

    eth image

--- components/ui/web3/ethRates/index.js
+++ components/ui/web3/ethRates/index.js
@@ -1,5 +1,5 @@
 
-
+import Image from "next/image"
 
 export default function EthRates({eth, ethPerItem}) {
 
@@ -7,16 +7,35 @@ export default function EthRates({eth, ethPerItem}) {
     <div className="grid grid-cols-4 mb-5">
       <div className="flex flex-1 items-stretch text-center">
         <div className="p-10 border drop-shadow rounded-md">
-          <div>
-            <span className="text-2xl font-bold">ETH = {eth}$</span>
+          <div className="flex items-center">
+            <Image
+              layout="fixed"
+              height="35"
+              width="35"
+              src="/small-eth.webp"
+            />
+            <span className="text-2xl font-bold">
+               = {eth}$
+            </span>
           </div>
           <p className="text-xl text-gray-500">Current eth Price</p>
         </div>
       </div>
       <div className="flex flex-1 items-stretch text-center">
         <div className="p-10 border drop-shadow rounded-md">
-          <div>
-            <span className="text-2xl font-bold">{ethPerItem} = 15$</span>
+          <div className="flex items-center">
+            <span className="text-2xl font-bold">
+              {ethPerItem}
+            </span>
+            <Image
+              layout="fixed"
+              height="35"
+              width="35"
+              src="/small-eth.webp"
+            />
+            <span className="text-2xl font-bold">
+              = 15$
+            </span>
           </div>
           <p className="text-xl text-gray-500">Price per course</p>
         </div>
