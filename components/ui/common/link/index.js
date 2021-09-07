cbf5d72c64287f4de909c2e09b44e0839975dce2
Filip Jerga <jerga99@gmail.com>
2021-09-07 09:21:57 +0200

    Active link 2

--- components/ui/common/link/index.js
+++ components/ui/common/link/index.js
@@ -1,10 +1,15 @@
 
 import Link from "next/link"
 import React from "react"
+import { useRouter } from "next/router"
 
-export default function ActiveLink({children, ...props}) {
-
+export default function ActiveLink({children, activeLinkClass, ...props}) {
+  const { pathname } = useRouter()
   let className = children.props.className || ""
+
+  if (pathname === props.href) {
+    className = `${className} ${activeLinkClass ? activeLinkClass : "text-indigo-600"}`
+  }
 
   return (
     <Link {...props}>
--- components/ui/common/navbar/index.js
+++ components/ui/common/navbar/index.js
@@ -2,7 +2,7 @@
 
 import { useWeb3 } from "@components/providers"
 import Link from "next/link"
-import { Button } from "@components/ui/common"
+import { ActiveLink, Button } from "@components/ui/common"
 import { useAccount } from "@components/hooks/web3"
 import { useRouter } from "next/router"
 
@@ -17,32 +17,32 @@ export default function Navbar() {
         <nav className="relative" aria-label="Global">
           <div className="flex justify-between items-center">
             <div>
-              <Link href="/" >
+              <ActiveLink href="/" >
                 <a
                   className="font-medium mr-8 text-gray-500 hover:text-gray-900">
                   Home
                 </a>
-              </Link>
-              <Link href="/marketplace" >
+              </ActiveLink>
+              <ActiveLink href="/marketplace" >
                 <a
                   className="font-medium mr-8 text-gray-500 hover:text-gray-900">
                   Marketplace
                 </a>
-              </Link>
-              <Link href="/" >
+              </ActiveLink>
+              <ActiveLink href="/blogs" >
                 <a
                   className="font-medium mr-8 text-gray-500 hover:text-gray-900">
                   Blogs
                 </a>
-              </Link>
+              </ActiveLink>
             </div>
             <div>
-              <Link href="/" >
+              <ActiveLink href="/wishlist" >
                 <a
                   className="font-medium mr-8 text-gray-500 hover:text-gray-900">
                   Wishlist
                 </a>
-              </Link>
+              </ActiveLink>
               { isLoading ?
                 <Button
                   disabled={true}
