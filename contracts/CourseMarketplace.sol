8a7e8dbd6257d666cfa09d85c91d98cbbba78fbf
Filip Jerga <jerga99@gmail.com>
2021-09-08 09:46:12 +0200

    Getter functions

--- contracts/CourseMarketplace.sol
+++ contracts/CourseMarketplace.sol
@@ -45,4 +45,28 @@ contract CourseMarketplace {
       state: State.Purchased
     });
   }
+
+  function getCourseCount()
+    external
+    view
+    returns (uint)
+  {
+    return totalOwnedCourses;
+  }
+
+  function getCourseHashAtIndex(uint index)
+    external
+    view
+    returns (bytes32)
+  {
+    return ownedCourseHash[index];
+  }
+
+  function getCourseByHash(bytes32 courseHash)
+    external
+    view
+    returns (Course memory)
+  {
+    return ownedCourses[courseHash];
+  }
 }
