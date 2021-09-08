cb1944e76713cbd90f7212948f3f1b5f62562bb7
Filip Jerga <jerga99@gmail.com>
2021-09-08 10:36:59 +0200

    Transfer ownership

--- contracts/CourseMarketplace.sol
+++ contracts/CourseMarketplace.sol
@@ -35,6 +35,16 @@ contract CourseMarketplace {
   /// Course has already a Owner!
   error CourseHasOwner();
 
+  /// Only owner has an access!
+  error OnlyOwner();
+
+  modifier onlyOwner() {
+    if (msg.sender != getContractOwner()) {
+      revert OnlyOwner();
+    }
+    _;
+  }
+
   function purchaseCourse(
     bytes16 courseId, // 0x00000000000000000000000000003130
     bytes32 proof // 0x0000000000000000000000000000313000000000000000000000000000003130
@@ -60,6 +70,13 @@ contract CourseMarketplace {
     });
   }
 
+  function transferOwnership(address newOwner)
+    external
+    onlyOwner
+  {
+    setContractOwner(newOwner);
+  }
+
   function getCourseCount()
     external
     view
@@ -82,6 +99,14 @@ contract CourseMarketplace {
     returns (Course memory)
   {
     return ownedCourses[courseHash];
+  }
+
+  function getContractOwner()
+    public
+    view
+    returns (address)
+  {
+    return owner;
   }
 
   function setContractOwner(address newOwner) private {
