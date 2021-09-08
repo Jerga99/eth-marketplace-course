e85f5f3329193cf7a7214a48492e06bc5b03fc0d
Filip Jerga <jerga99@gmail.com>
2021-09-08 09:18:07 +0200

    Course hash

--- contracts/CourseMarketplace.sol
+++ contracts/CourseMarketplace.sol
@@ -16,4 +16,22 @@ contract CourseMarketplace {
     address owner; // 20
     State state; // 1
   }
+
+  function purchaseCourse(
+    bytes16 courseId,
+    bytes32 proof // 0x0000000000000000000000000000313000000000000000000000000000003130
+  )
+    external
+    payable
+    returns(bytes32)
+  {
+    // course id - 10
+    // 0x00000000000000000000000000003130
+    // 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
+    // 000000000000000000000000000031305B38Da6a701c568545dCfcB03FcB875f56beddC4
+    // keccak256 - c4eaa3558504e2baa2669001b43f359b8418b44a4477ff417b4b007d7cc86e37
+              // - c4eaa3558504e2baa2669001b43f359b8418b44a4477ff417b4b007d7cc86e37
+    bytes32 courseHash = keccak256(abi.encodePacked(courseId, msg.sender));
+    return courseHash;
+  }
 }
