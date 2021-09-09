a2a59f0ca5e7820b198c35701aed3f8143f9d895
Filip Jerga <jerga99@gmail.com>
2021-09-09 10:48:19 +0200

    Prepare values

--- components/providers/web3/index.js
+++ components/providers/web3/index.js
@@ -23,7 +23,7 @@ export default function Web3Provider({children}) {
       if (provider) {
         const web3 = new Web3(provider)
         const contract = await loadContract("CourseMarketplace", web3)
-        console.log(contract)
+
         setWeb3Api({
           provider,
           web3,
