17feb8784d7094e8aa7d4b9ffb5049759e42e850
Filip Jerga <jerga99@gmail.com>
2021-09-10 11:55:03 +0200

    Course card styles

--- components/ui/course/card/OwnedCourseCard.js
+++ components/ui/course/card/OwnedCourseCard.js
@@ -1,40 +1,56 @@
 
+import Image from "next/image"
 
 export default function OwnedCourseCard({children, course}) {
 
   return (
     <div className="bg-white border shadow overflow-hidden sm:rounded-lg mb-3">
-      <div className="px-4 py-5 sm:px-6">
-        <h3 className="text-lg leading-6 font-medium text-gray-900">
-          {course.title}
-        </h3>
-        <p className="mt-1 max-w-2xl text-sm text-gray-500">
-        {course.price} ETH
-        </p>
-      </div>
+      <div className="flex">
+        <div className="flex-1">
+          <div className="h-full next-image-wrapper">
+            <Image
+              className="object-cover"
+              src={course.coverImage}
+              width="45"
+              height="45"
+              layout="responsive"
+            />
+          </div>
+        </div>
+        <div className="flex-4">
+          <div className="px-4 py-5 sm:px-6">
+            <h3 className="text-lg leading-6 font-medium text-gray-900">
+              {course.title}
+            </h3>
+            <p className="mt-1 max-w-2xl text-sm text-gray-500">
+            {course.price} ETH
+            </p>
+          </div>
 
-      <div className="border-t border-gray-200">
-        <dl>
-          <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-9 sm:gap-4 sm:px-6">
-            <dt className="text-sm font-medium text-gray-500">
-              Course ID
-            </dt>
-            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-              {course.ownedCourseId}
-            </dd>
+          <div className="border-t border-gray-200">
+            <dl>
+              <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-9 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">
+                  Course ID
+                </dt>
+                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                  {course.ownedCourseId}
+                </dd>
+              </div>
+              <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-9 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">
+                  Proof
+                </dt>
+                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                  {course.proof}
+                </dd>
+              </div>
+              <div className="bg-white px-4 py-5 sm:px-6">
+                {children}
+              </div>
+            </dl>
           </div>
-          <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-9 sm:gap-4 sm:px-6">
-            <dt className="text-sm font-medium text-gray-500">
-              Proof
-            </dt>
-            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-              {course.proof}
-            </dd>
-          </div>
-          <div className="bg-white px-4 py-5 sm:px-6">
-            {children}
-          </div>
-        </dl>
+        </div>
       </div>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,9 @@ module.exports = {
   theme: {
     extend: {
       flex: {
-        "2": "2 2 0%"
+        "2": "2 2 0%",
+        "3": "3 3 0%",
+        "4": "4 4 0%"
       },
       maxWidth: {
         "8xl": "1920px"
