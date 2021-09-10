01840a0e29a896327f3409d89139ed03a84dd7a5
Filip Jerga <jerga99@gmail.com>
2021-09-10 09:25:07 +0200

    Create web3 state

--- components/providers/web3/index.js
+++ components/providers/web3/index.js
@@ -7,14 +7,25 @@ import { setupHooks } from "./hooks/setupHooks";
 
 const Web3Context = createContext(null)
 
+const createWeb3State = ({web3, provider, contract, isLoading}) => {
+  return {
+    web3,
+    provider,
+    contract,
+    isLoading,
+    hooks: setupHooks({web3, provider, contract})
+  }
+}
+
 export default function Web3Provider({children}) {
-  const [web3Api, setWeb3Api] = useState({
-    provider: null,
-    web3: null,
-    contract: null,
-    isLoading: true,
-    hooks: setupHooks({provider: null, web3: null, contract: null})
-  })
+  const [web3Api, setWeb3Api] = useState(
+    createWeb3State({
+      web3: null,
+      provider: null,
+      contract: null,
+      isLoading: true
+    })
+  )
 
   useEffect(() => {
     const loadProvider = async () => {
@@ -24,13 +35,14 @@ export default function Web3Provider({children}) {
         const web3 = new Web3(provider)
         const contract = await loadContract("CourseMarketplace", web3)
 
-        setWeb3Api({
-          provider,
-          web3,
-          contract,
-          isLoading: false,
-          hooks: setupHooks({web3, provider, contract})
-        })
+        setWeb3Api(
+          createWeb3State({
+            web3,
+            provider,
+            contract,
+            isLoading: false
+          })
+        )
       } else {
         setWeb3Api(api => ({...api, isLoading: false}))
         console.error("Please, install Metamask.")
