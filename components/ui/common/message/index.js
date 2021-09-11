65a8f4b0d8f522bad59b996f3912ff8a7c1d1048
Filip Jerga <jerga99@gmail.com>
2021-09-11 16:13:31 +0200

    Course state message

--- components/ui/common/message/index.js
+++ components/ui/common/message/index.js
@@ -20,7 +20,7 @@ export default function Message({children, type = "success"}) {
       <div className="max-w-7xl mx-auto py-3 px-3 sm:px-3 lg:px-3">
         <div className="flex items-center justify-between flex-wrap">
           <div className="w-0 flex-1 flex items-center">
-            <p className={`ml-3 font-medium text-${messageType}-900 truncate`}>
+            <p className={`ml-3 font-medium text-${messageType}-900`}>
               <span className="hidden md:inline">
                 { children }
               </span>
--- pages/courses/[slug].js
+++ pages/courses/[slug].js
@@ -1,5 +1,5 @@
 import { useAccount, useOwnedCourse } from "@components/hooks/web3";
-import { Modal } from "@components/ui/common";
+import { Message, Modal } from "@components/ui/common";
 import {
   CourseHero,
   Curriculum,
@@ -11,6 +11,8 @@ import { getAllCourses } from "@content/courses/fetcher";
 export default function Course({course}) {
   const { account } = useAccount()
   const { ownedCourse } = useOwnedCourse(course, account.data)
+  const courseState = ownedCourse.data?.state
+  // const courseState = "deactivated"
 
   return (
     <>
@@ -25,6 +27,28 @@ export default function Course({course}) {
       <Keypoints
         points={course.wsl}
       />
+      { courseState &&
+        <div className="max-w-5xl mx-auto">
+          { courseState === "purchased" &&
+            <Message type="warning">
+              Course is purchased and waiting for the activation. Process can take up to 24 hours.
+              <i className="block font-normal">In case of any questions, please contact info@eincode.com</i>
+            </Message>
+          }
+          { courseState === "activated" &&
+            <Message type="success">
+              Eincode wishes you happy watching of the course.
+            </Message>
+          }
+          { courseState === "deactivated" &&
+            <Message type="danger">
+              Course has been deactivated, due the incorrect purchase data.
+              The functionality to watch the course has been temporaly disabled.
+              <i className="block font-normal">Please contact info@eincode.com</i>
+            </Message>
+          }
+        </div>
+      }
       <Curriculum
         locked={true}
       />
