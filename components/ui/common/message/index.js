ca32b786c57d5d2cf1cb96ad11f331ed71a3b2c9
Filip Jerga <jerga99@gmail.com>
2021-09-13 10:02:37 +0200

    Check for no courses

--- components/ui/common/message/index.js
+++ components/ui/common/message/index.js
@@ -20,11 +20,11 @@ export default function Message({children, type = "success"}) {
       <div className="max-w-7xl mx-auto py-3 px-3 sm:px-3 lg:px-3">
         <div className="flex items-center justify-between flex-wrap">
           <div className="w-0 flex-1 flex items-center">
-            <p className={`ml-3 font-medium text-${messageType}-900`}>
-              <span className="hidden md:inline">
+            <div className={`ml-3 font-medium text-${messageType}-900`}>
+              <span className="inline">
                 { children }
               </span>
-            </p>
+            </div>
           </div>
           <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
             <button
--- pages/marketplace/courses/owned.js
+++ pages/marketplace/courses/owned.js
@@ -1,12 +1,13 @@
 
 
-import { useAccount, useOwnedCourses } from "@components/hooks/web3";
-import { Button, Message } from "@components/ui/common";
-import { OwnedCourseCard } from "@components/ui/course";
-import { BaseLayout } from "@components/ui/layout";
-import { MarketHeader } from "@components/ui/marketplace";
-import { getAllCourses } from "@content/courses/fetcher";
-import { useRouter } from "next/router";
+import { useAccount, useOwnedCourses } from "@components/hooks/web3"
+import { Button, Message } from "@components/ui/common"
+import { OwnedCourseCard } from "@components/ui/course"
+import { BaseLayout } from "@components/ui/layout"
+import { MarketHeader } from "@components/ui/marketplace"
+import { getAllCourses } from "@content/courses/fetcher"
+import { useRouter } from "next/router"
+import Link from "next/link"
 
 export default function OwnedCourses({courses}) {
   const router = useRouter()
@@ -17,14 +18,25 @@ export default function OwnedCourses({courses}) {
     <>
       <MarketHeader />
       <section className="grid grid-cols-1">
+        { ownedCourses.hasInitialResponse && (
+          !ownedCourses.data || ownedCourses?.data.length === 0
+        ) &&
+          <div className="w-1/2">
+            <Message type="warning">
+              <div>You don't own any courses</div>
+              <Link href="/marketplace">
+                <a className="font-normal hover:underline">
+                  <i>Purchase Course</i>
+                </a>
+              </Link>
+            </Message>
+          </div>
+        }
         { ownedCourses.data?.map(course =>
           <OwnedCourseCard
             key={course.id}
             course={course}
           >
-            {/* <Message>
-              My custom message!
-            </Message> */}
             <Button
               onClick={() => router.push(`/courses/${course.slug}`)}
             >
