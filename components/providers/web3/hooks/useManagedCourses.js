0913c89bf2072b853c92880bfd268040573eb651
Filip Jerga <jerga99@gmail.com>
2021-09-13 11:48:56 +0200

    Commit fetch managed courses

--- components/providers/web3/hooks/useManagedCourses.js
+++ components/providers/web3/hooks/useManagedCourses.js
@@ -8,7 +8,18 @@ export const handler = (web3, contract) => account => {
   const swrRes = useSWR(() =>
     (web3 && contract && account) ? `web3/managedCourses/${account}` : null,
     async () => {
-      const courses = [1,2,3,4]
+      const courses = []
+      const courseCount = await contract.methods.getCourseCount().call()
+
+      for (let i = Number(courseCount) - 1; i >= 0; i--) {
+        const courseHash = await contract.methods.getCourseHashAtIndex(i).call()
+        const course = await contract.methods.getCourseByHash(courseHash).call()
+
+        if (course) {
+          const normalized = normalizeOwnedCourse(web3)({ hash: courseHash }, course)
+          courses.push(normalized)
+        }
+      }
 
       return courses
     }
