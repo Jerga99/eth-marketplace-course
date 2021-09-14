be126610e9fbf568914e1de04128228bfaf931e3
Filip Jerga <jerga99@gmail.com>
2021-09-14 09:31:24 +0200

    Owned card values

--- components/ui/course/card/Managed.js
+++ components/ui/course/card/Managed.js
@@ -1,9 +1,9 @@
 
 
-const Item = ({title, value}) => {
+const Item = ({title, value, className}) => {
 
   return (
-    <div className="bg-gray-50 px-4 py-5  sm:px-6">
+    <div className={`${className} px-4 py-5  sm:px-6`}>
       <div className="text-sm font-medium text-gray-500">
         {title}
       </div>
@@ -20,14 +20,15 @@ export default function ManagedCourseCard({children, course}) {
   return (
     <div className="bg-white border shadow overflow-hidden sm:rounded-lg mb-3">
       <div className="border-t border-gray-200">
-        <Item
-          title="Course ID"
-          value={course.ownedCourseId}
-        />
-        <Item
-          title="Proof"
-          value={course.proof}
-        />
+        { Object.keys(course).map((key, i) =>
+          <Item
+            key={key}
+            className={`${i % 2 ? "bg-gray-50" : "bg-white"}`}
+            // title={key}
+            title={key[0].toUpperCase() + key.slice(1)}
+            value={course[key]}
+          />
+        )}
         <div className="bg-white px-4 py-5 sm:px-6">
           {children}
         </div>
