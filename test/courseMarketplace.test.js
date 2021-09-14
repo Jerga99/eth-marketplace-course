dd9f4af138bab5c3b3092e4a9045dd0606c8f321
Filip Jerga <jerga99@gmail.com>
2021-09-14 12:51:30 +0200

    Test course by index

--- test/courseMarketplace.test.js
+++ test/courseMarketplace.test.js
@@ -6,6 +6,10 @@ const CourseMarketplace = artifacts.require("CourseMarketplace")
 
 contract("CourseMarketplace", accounts => {
 
+  const courseId = "0x00000000000000000000000000003130";
+  const proof = "0x0000000000000000000000000000313000000000000000000000000000003130"
+  const value = "900000000";
+
   let _contract = null
   let contractOwner = null
   let buyer = null
@@ -17,8 +21,22 @@ contract("CourseMarketplace", accounts => {
   })
 
   describe("Purchase the new course", () => {
-    it("should resolve into true value", () => {
-      assert(true, "Value is NOT true")
+    before(async() => {
+      await _contract.purchaseCourse(courseId, proof, {
+        from: buyer,
+        value
+      })
+    })
+
+    it("can get the purchased course hash by index", async () => {
+      const index = 0
+      const courseHash = await _contract.getCourseHashAtIndex(index)
+      const expectedHash = web3.utils.soliditySha3(
+        { type: "bytes16", value: courseId },
+        { type: "address", value: buyer },
+      )
+
+      assert.equal(courseHash, expectedHash, "Course hash is not maching the hash of purchased course!")
     })
   })
 })
