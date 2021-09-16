744e48a03d0b741e8cdbdb2c8c792a4dc2fbe00d
Filip Jerga <jerga99@gmail.com>
2021-09-16 18:29:48 +0200

    Test repurchase

--- test/courseMarketplace.test.js
+++ test/courseMarketplace.test.js
@@ -139,6 +139,37 @@ contract("CourseMarketplace", accounts => {
     it("should NOT be able activate deactivated course", async () => {
       await catchRevert(_contract.activateCourse(courseHash2, {from: contractOwner}))
     })
+  })
+
+  describe("Repurchase course", () => {
+    let courseHash2 = null
+
+    before(async () => {
+      courseHash2 = await _contract.getCourseHashAtIndex(1)
+    })
+
+    it("should NOT repurchase when the course doesn't exist", async () => {
+      const notExistingHash = "0x5ceb3f8075c3dbb5d490c8d1e6c950302ed065e1a9031750ad2c6513069e3fc3"
+      await catchRevert(_contract.repurchaseCourse(notExistingHash, {from: buyer}))
+    })
+
+    it("should NOT repurchase with NOT course owner", async () => {
+      const notOwnerAddress = accounts[2]
+      await catchRevert(_contract.repurchaseCourse(courseHash2, {from: notOwnerAddress}))
+    })
+
+    it("should be able repurchase with the original buyer", async () => {
+      await _contract.repurchaseCourse(courseHash2, {from: buyer, value})
+      const course = await _contract.getCourseByHash(courseHash2)
+      const exptectedState = 0
+
+      assert.equal(course.state, exptectedState, "The course is not in purchased state")
+      assert.equal(course.price, value, `The course price is not equal to ${value}`)
+    })
+
+    it("should NOT be able to repurchase purchased course", async () => {
+      await catchRevert(_contract.repurchaseCourse(courseHash2, {from: buyer}))
+    })
 
   })
 
