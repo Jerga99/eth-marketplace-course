b46bb424fc145d387f2089fae9a5b5c830122b8c
Filip Jerga <jerga99@gmail.com>
2021-09-21 13:12:15 +0200

    Display toast with tx

--- pages/marketplace/index.js
+++ pages/marketplace/index.js
@@ -35,9 +35,9 @@ export default function Marketplace({courses}) {
         { type: "bytes32", value: orderHash }
       )
 
-      _purchaseCourse(hexCourseId, proof, value)
+      withToast(_purchaseCourse(hexCourseId, proof, value))
     } else {
-      _repurchaseCourse(orderHash, value)
+      withToast(_repurchaseCourse(orderHash, value))
     }
   }
 
@@ -47,9 +47,10 @@ export default function Marketplace({courses}) {
         hexCourseId,
         proof
       ).send({from: account.data, value})
-      console.log(result)
-    } catch {
-      console.error("Purchase course: Operation has failed.")
+
+      return result
+    } catch(error) {
+      throw new Error(error.message)
     }
   }
 
@@ -58,28 +59,16 @@ export default function Marketplace({courses}) {
       const result = await contract.methods.repurchaseCourse(
         courseHash
       ).send({from: account.data, value})
-      console.log(result)
-    } catch {
-      console.error("Purchase course: Operation has failed.")
+
+      return result
+    } catch(error) {
+      throw new Error(error.message)
     }
-  }
-
-  const notify = () => {
-    // const resolveWithSomeData = new Promise(resolve => setTimeout(() => resolve({
-    //   transactionHash: "0x610ebf769141514a711bb9ef01c09340f14fe28c3709a3c12c0c05dd5e7c668a"
-    // }), 3000));
-    const resolveWithSomeData = new Promise(
-      (resolve, reject) => setTimeout(() => reject(new Error("Some Error")), 3000))
-
-    withToast(resolveWithSomeData)
   }
 
   return (
     <>
       <MarketHeader />
-      <Button onClick={notify}>
-        Notify!
-      </Button>
       <CourseList
         courses={courses}
       >
